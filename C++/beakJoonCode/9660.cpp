cdc6446db75bc6eef74c0fcc02a53b9a0fc20a38
송재욱 <123460320+976520@users.noreply.github.com>
2024-06-26 15:17:20 +0900

    Create 9660.cpp

---
+++ C++/beakJoonCode/9660.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	long long n;
+	
+	cin >> n;
+	
+	if (n % 7 == 0 || n % 7 == 2) {
+		cout << "CY" << endl;
+	} else {
+		cout << "SK" << endl;
+	}
+
+	return 0;
+}
